fc87d8d98d7dffab3147bc48325a8ebe3acd2657
Shilpe <penguns@naver.com>
2012-12-09 10:41:00 +0900

    Change websocket engine

--- app.js
+++ app.js
@@ -1,37 +1,66 @@
-/**
- * Module dependencies.
- */
+var logo = '\n'
++'================================================================\n\n'
++'                        S C R A P E R                           \n\n'
++'----------------------------------------------------------------\n'
++'          AMI TEAM PROJECT : ENTERTAINMENT WEB CRAWLER          \n'
++'================================================================\n\n';
+console.log(logo);
+// Rinker for web crawler
+// Rinekr : https://github.com/HanSeungHo/rinker
 
-var express = require('express')
-  , routes = require('./routes')
-  , user = require('./routes/user')
-  , http = require('http')
-  , path = require('path');
+var io = require('socket.io').listen(3100);
+var scrap = require('./lib/scrap');
+var mysql = require('./lib/mysql');
 
-var app = express();
+// Debug level
+io.set('log level', 1);
 
-app.configure(function(){
-  app.set('port', process.env.PORT || 3001);
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
-  app.use(express.favicon());
-  app.use(express.logger('dev'));
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(app.router);
-  app.use(express.static(path.join(__dirname, 'public')));
+// Websocket Server
+io.sockets.on('connection', function (client) {
+
+	console.log('Rinker Socket.io client connected'); 
+	client.emit('console', ' == Scraper Socket.io server connected');
+	// people.nate.com page scrape
+	client.on('nate list', function (data) {
+
+		var start = data.start;
+		var end = data.end;
+		console.log(start, end);
+		if (start && end) {
+			for (var i = start; i <= end; i++) {
+				 scrap.nate_listParsing('00010011', i);
+				 client.emit('console', ' ++ people.nate.com : 페이지 ' + i + '수집');
+			}
+		}
+		else {
+			client.emit('console', ' -- Error: 수집할 페이지 시작과 끝을 입력해주세요.');
+		};
+
+	});
+
+	// people.nate.com person scrape
+	client.on('nate person', 
+		function (data) {
+
+			var start = data.start;
+			var end = data.end;
+			console.log(start, end);
+			if (start && end){
+				client.emit('console', ' ++ people.nate.com : 인물 id:' + start + '~' + end + ' 수집');
+			  mysql.getPersonid(start, end,
+				function (results) {
+					for (x in results){
+						scrap.nate_personParsing(results[x].id, results[x].nate_url);
+			 			client.emit('console', ' ++ people.nate.com : 인물 id:' + results[x].id
+			 									+ " URL: <a href='" + results[x].nate_url + "'>" + results[x].nate_url + "</a> 수집");
+      		} 
+			  });
+			}
+			else {
+				client.emit('console', ' -- Error: 수집할 인물 시작과 끝을 입력해주세요.');
+			};
+		}
+	);
+
 });
 
-app.configure('development', function(){
-  app.use(express.errorHandler());
-});
-
-app.get('/', routes.index);
-app.get('/getlist', routes.getList);
-app.get('/getpeople', routes.getPeople);
-app.get('/natelist', routes.nate_listParsing);
-app.get('/users', user.list);
-
-http.createServer(app).listen(app.get('port'), function(){
-  console.log("Express server listening on port " + app.get('port'));
-});
--- config/db.js
+++ config/db.js
@@ -1,12 +1,61 @@
-exports.mysql = { 
-	host : 'ami.hansh.kr',
-	port : '3406',
-	user : 'test',
-	password : 'ttest'	
+// exports.mysql = { 
+// 	host : 'rinker.kr',
+// 	port : '3406',
+// 	user : 'test',
+// 	password : 'ttest'	
+// };
+
+// exports.test = '테스트';
+
+// exports.people_db = 'repository';
+// exports.peoplelist_table = 'people'
+// exports.people_table = 'nate_people';
+
+// MySQL database
+exports.MYSQL = {
+	CLIENT : {
+		host : 'ami.hansh.kr',
+		port : '3406',
+		user : 'test',
+		password : 'ttest'
+	},
+	PERSON : {
+		DB    : 'repository',
+		TABLE : 'people',
+		LIMIT : 40
+	},
+	MUSIC : {
+		DB    : 'repository',
+		TABLE : 'nate_people'
+	},
+	MOVIE : {
+		DB    : 'repository',
+		TABLE : 'nate_people'
+	},
+	MAIN : {
+		DB : 'ent',
+		MUSIC : 'music',
+		MOVIE : 'movie',
+		PERSON : 'person',
+	},
+	QUERY : {
+		DB    : 'log',
+		TABLE : 'query'
+	}
 };
 
-exports.test = '테스트';
-
-exports.people_db = 'repository';
-exports.peoplelist_table = 'people'
-exports.people_table = 'nate_people';
+// Neo4J databse
+exports.NEO4J = {
+	HOST : 'http://203.247.161.50:7474',
+	NODE : 'node',
+	REL  : {  
+		MUSIC : 'music',
+		MOVIE : 'movie',
+		ISSUE : 'isuue'
+	},
+	VAL  : { 
+		NAME : 'name',
+		BIRTH : 'birth',
+		THUMB : 'thumb' 
+	}
+}
